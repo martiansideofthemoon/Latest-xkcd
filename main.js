61c1e5257845f2039e68052471d42ae1431171a2
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-05-09 12:40:14 +0530

    Random function fixed

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ fileref.setAttribute("type","text/javascript")
 fileref.setAttribute("src", "https://dynamic.xkcd.com/api-0/jsonp/comic/"+num+"?callback=storeComic")
 
 function loadRandom(){
-    num = Math.floor(Math.random()*parseInt(num))
+	var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+	var firstDate = new Date(2015,04,2);
+	var secondDate = new Date();
+	var diffDays = Math.floor(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+	numComics=1522+parseInt((diffDays+5)/7)+parseInt((diffDays+3)/7)+parseInt((diffDays+1)/7)
+    num = Math.floor(Math.random()*parseInt(numComics+1))
     window.open( "xkcd.htm?"+num,"_self")
 } 
 
